abf36ef1177d04cbcaaa7e69133ff0382beacee8
Stefan Koch <code@unstko.de>
2016-12-10 16:15:45 +0100

    Base class for Unit Tests
    Sets up and tears down calculation of a solution for a day
    Mocks the input from the input.txt by an input string

---
+++ lib/test.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import mock_open, patch
+import solution
+
+
+class Test(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(Test, self).__init__(*args, **kwargs)
+        self.nr = ""
+
+    def setUp(self):
+        self.solution = solution.Solution(self.nr)
+
+    def tearDown(self):
+        print(self._testMethodName+":")
+        print(self.solution)
+        print("")
+
+    def execute_test(self, test_input):
+        mocked_open_function = mock_open(read_data=test_input)
+        with patch("builtins.open", mocked_open_function):
+            self.solution.calculate()
+
+    def get_solution(self, nr):
+        return self.solution.get_solution(nr)
