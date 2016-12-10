ca2517cab74a4cf4ed87fb27045c506fad4f7a54
Stefan Koch <code@unstko.de>
2016-12-10 16:08:26 +0100

    Base class for Solutions
    Solution for day "nr" can calculate part solutions 1 and 2
    Input is loaded from file "input.txt" inside the folder of the day
    Solutions of a day have to define their own calculate method

---
+++ lib/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def __init__(self, nr):
+        self.nr = nr
+        self.input = ""
+        self.solution = ["(not calculated)", "(not calculated)"]
+        self.calculated = [False, False]
+
+    def __str__(self):
+        return "Solution 1: {}\nSolution 2: {}".format(self.solution[0], self.solution[1])
+
+    def calculate(self):
+        raise NotImplementedError('users must define calculate to use this base class')
+
+    def get_solution(self, nr):
+        if nr in [1, 2]:
+            return self.solution[nr-1]
+
+    def set_solution(self, nr, value):
+        if nr in [1, 2]:
+            self.solution[nr-1] = value
+            self.calculated[nr-1] = True
+
+    def is_calculated(self, nr):
+        if nr in [1, 2]:
+            return self.calculated[nr-1]
+
+    def read_input(self):
+        with open(self.nr+"/input.txt", "r") as f:
+            self.input = f.read()
