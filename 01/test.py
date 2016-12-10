662a4a6f980e28e696620b24ec000dd63f31519b
Stefan Koch <code@unstko.de>
2016-12-10 21:03:07 +0100

    Day 01: Swap unit test assert arguments
    First argument is expected, second is actual

--- 01/test.py
+++ 01/test.py
@@ -9,19 +9,19 @@ class Test01(Test):
     def test_1(self):
         test_input = "R2, L3"
         self.execute_test(test_input)
-        self.assertEqual(self.solution.get_solution(1), 5)
+        self.assertEqual(5, self.solution.get_solution(1))
 
     def test_2(self):
         test_input = "R2, R2, R2"
         self.execute_test(test_input)
-        self.assertEqual(self.get_solution(1), 2)
+        self.assertEqual(2, self.get_solution(1))
 
     def test_3(self):
         test_input = "R5, L5, R5, R3"
         self.execute_test(test_input)
-        self.assertEqual(self.get_solution(1), 12)
+        self.assertEqual(12, self.get_solution(1))
 
     def test_4(self):
         test_input = "R8, R4, R4, R8"
         self.execute_test(test_input)
-        self.assertEqual(self.get_solution(2), 4)
+        self.assertEqual(4, self.get_solution(2))
