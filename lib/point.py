d148e8e6c8e3c66a9d9cc70bd11b020654ce747e
Stefan Koch <code@unstko.de>
2016-12-10 16:18:43 +0100

    2D point class
    Set and get x and y coordinates
    Print point coordinates
    Add, sub and neg points
    Calc euclidean and manhattan distances between points

---
+++ lib/point.py
@@ -0,0 +1,38 @@
+import math
+
+
+class Point2D:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __str__(self):
+        return "{}, {}".format(self.x, self.y)
+
+    def __neg__(self):
+        return Point2D(-self.x, -self.y)
+
+    def __add__(self, point):
+        return Point2D(self.x+point.x, self.y+point.y)
+
+    def __sub__(self, point):
+        return self + -point
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def move(self, dx, dy):
+        self.x = self.x + dx
+        self.y = self.y + dy
+
+    def euclidean_distance(self, point):
+        dx = self.x - point.x
+        dy = self.y - point.y
+        return math.hypot(dx, dy)
+
+    def manhattan_distance(self, point):
+        dx_abs = math.fabs(self.x - point.x)
+        dy_abs = math.fabs(self.y - point.y)
+        return int(dx_abs + dy_abs)
