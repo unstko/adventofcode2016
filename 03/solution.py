c04ff30b1c1bc24f7bbea8df1252331acc24f2f9
Stefan Koch <code@unstko.de>
2016-12-11 14:36:56 +0100

    Day 03: Solution of part 2

--- 03/solution.py
+++ 03/solution.py
@@ -8,6 +8,8 @@ class Solution(solution.Solution):
         super().__init__(nr)
         self.triangle_1 = Triangle()
         self.possible_1 = 0
+        self.triangles_2 = [Triangle(), Triangle(), Triangle()]
+        self.possible_2 = 0
 
     def calculate(self, test=False):
         self.read_instructions()
@@ -17,9 +19,18 @@ class Solution(solution.Solution):
         self.read_input(True)
 
     def check_triangles(self):
+        group_part = 0
         for triangle in self.input:
             sides = re.split(r'\s+', triangle.strip(' '))
             self.triangle_1.set_sides(sides)
             if self.triangle_1.is_possible():
                 self.possible_1 += 1
+            for nr in range(0, 3):
+                self.triangles_2[nr].set_side(group_part, sides[nr])
+            if group_part == 2:
+                for nr in range(0, 3):
+                    if self.triangles_2[nr].is_possible():
+                        self.possible_2 += 1
+            group_part = (group_part + 1) % 3
         self.set_solution(1, self.possible_1)
+        self.set_solution(2, self.possible_2)
