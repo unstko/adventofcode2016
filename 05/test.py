6f2f378c397c6e7d9b3f6df4819310f2c411820f
Stefan Koch <code@unstko.de>
2016-12-15 21:18:15 +0100

    Day 05: Initial commit

---
+++ 05/test.py
@@ -0,0 +1,12 @@
+from lib import test
+
+
+class Test(test.Test):
+    @classmethod
+    def setUpClass(cls):
+        cls.nr = '05'
+
+    def test_1(self):
+        test_input = "abc"
+        self.execute_test(test_input)
+        self.assertEqual("18f47a30", self.solution.get_solution(1))
