005a089f7225eaed83e52eb6fc8084fa3977e14c
Stefan Koch <code@unstko.de>
2016-12-15 21:43:55 +0100

    Day 05: Solution of part 1

--- 05/solution.py
+++ 05/solution.py
@@ -1,12 +1,30 @@
 from lib import solution
+import hashlib
 
 
 class Solution(solution.Solution):
     def __init__(self, nr):
         super().__init__(nr)
+        self.password = ""
 
     def calculate(self, test=False):
         self.read_instructions()
 
     def read_instructions(self):
         self.read_input()
+        self.calc_password()
+        self.set_solution(1, self.password)
+
+    def calc_password(self):
+        index = -1
+        m = hashlib.md5()
+        m.update(self.input.encode('utf-8'))
+        for i in range(0, 8):
+            while True:
+                index += 1
+                mc = m.copy()
+                mc.update(str(index).encode('utf-8'))
+                md5 = mc.hexdigest()
+                if md5[:5] == "00000":
+                    self.password += md5[5]
+                    break
