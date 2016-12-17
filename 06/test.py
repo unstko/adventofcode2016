087e5d0d66b246651b0300f50b17c27c8c93d38d
Stefan Koch <code@unstko.de>
2016-12-17 16:18:17 +0100

    Day 06: Initial commit

---
+++ 06/test.py
@@ -0,0 +1,29 @@
+from lib import test
+
+
+class Test(test.Test):
+    @classmethod
+    def setUpClass(cls):
+        cls.nr = '06'
+
+    def test_1(self):
+        test_input = """
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar
+        """
+        self.execute_test(test_input)
+        self.assertEqual("easter", self.solution.get_solution(1))
