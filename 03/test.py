e1f671559adb1cdce34cbef2e20b6e45dfd5218d
Stefan Koch <code@unstko.de>
2016-12-17 17:44:55 +0100

    Day 03: Unit test with positive test

--- 03/test.py
+++ 03/test.py
@@ -10,3 +10,8 @@ class Test03(Test):
         test_input = "5 10 25"
         self.execute_test(test_input)
         self.assertEqual(0, self.solution.get_solution(1))
+
+    def test_2(self):
+        test_input = "20 10 25"
+        self.execute_test(test_input)
+        self.assertEqual(1, self.solution.get_solution(1))
